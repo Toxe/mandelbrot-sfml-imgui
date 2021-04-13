9e62e84386991a3e49009bf167c9c233bea88fa3
Toxe <toxe@toxe.net>
2021-04-13 15:53:21 +0200

    Use .resize() to resize iterations_histogram

--- src/worker.cpp
+++ src/worker.cpp
@@ -4,6 +4,14 @@
 
 #include <spdlog/spdlog.h>
 
+void worker_resize_iterations_histogram_if_needed(const int id, const WorkerCalc& calc, std::vector<int>& iterations_histogram)
+{
+    if (std::ssize(iterations_histogram) != calc.max_iterations + 1) {
+        spdlog::debug("worker {}: resize iterations_histogram {} --> {}", id, iterations_histogram.size(), calc.max_iterations + 1);
+        iterations_histogram.resize(static_cast<std::size_t>(calc.max_iterations + 1));
+    }
+}
+
 void worker(const int id, std::mutex& mtx, std::condition_variable& cv, std::queue<WorkerMessage>& worker_message_queue, std::queue<SupervisorMessage>& supervisor_message_queue)
 {
     spdlog::debug("worker {}: started", id);
@@ -26,12 +34,7 @@ void worker(const int id, std::mutex& mtx, std::condition_variable& cv, std::que
             break;
         } else if (std::holds_alternative<WorkerCalc>(msg)) {
             WorkerCalc calc{std::get<WorkerCalc>(msg)};
-
-            if (std::ssize(iterations_histogram) != calc.max_iterations + 1) {
-                spdlog::debug("worker {}: iterations_histogram {} --> {}", id, iterations_histogram.size(), calc.max_iterations + 1);
-                iterations_histogram = std::vector<int>(static_cast<std::size_t>(calc.max_iterations + 1));
-            }
-
+            worker_resize_iterations_histogram_if_needed(id, calc, iterations_histogram);
             mandelbrot_calc(calc.image_size, calc.fractal_section, calc.max_iterations, iterations_histogram, *calc.results_per_point, calc.area);
 
             {
