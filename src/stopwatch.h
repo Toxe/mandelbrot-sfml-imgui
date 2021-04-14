fd24b25501ec41dd8d56d03ca143f31b2055b463
Toxe <toxe@toxe.net>
2021-04-14 21:06:13 +0200

    Add Stopwatch class

---
+++ src/stopwatch.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <SFML/System/Clock.hpp>
+
+class Stopwatch {
+    bool running_ = false;
+    float elapsed_time_ = 0.0;
+    sf::Clock clock_;
+
+public:
+    bool is_running() const { return running_; }
+
+    void start()
+    {
+        running_ = true;
+        elapsed_time_ = 0.0;
+        clock_.restart();
+    }
+
+    void stop()
+    {
+        running_ = false;
+        elapsed_time_ = clock_.getElapsedTime().asSeconds();
+    }
+
+    float time()
+    {
+        if (!running_)
+            return elapsed_time_;
+
+        return elapsed_time_ = clock_.getElapsedTime().asSeconds();
+    }
+};
